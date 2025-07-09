85ee790d4afe964450981af82b9855e9644eb37f
hajime <hima.0432@gmail.com>
2025-07-09 15:32:57 -0700

    publishing feature

--- data_management.py
+++ data_management.py
@@ -15,9 +15,20 @@ def view_transactions_by_date(df: pd.DataFrame, start: str, end: str) -> None:
     """Filters and displays transactions between two dates (YYYY-MM-DD)."""
     pass
 
-def add_transaction(df: pd.DataFrame, date: str, category: str, description: str, amount: float) -> pd.DataFrame:
-    """Adds a transaction and returns the updated DataFrame."""
-    pass
+def add_a_transaction(df):
+    date = input("Enter the date(YYYY-MM-DD): ")
+    category = input("Enter the category of the transaction: ")
+    description = input("Enter the description of the transaction: ")
+    amount = input("Enter the amount of the transaction: ")
+    type = input("Enter the type of the transaction(Expense or Income): ")
+
+    new_row = {"Date":date,"Category":category,"Description":description, "Amount":amount, "Type":type}
+    df.loc[len(df)] = new_row
+    df = df.sort_values(by = ["Date"],ascending = True, ignore_index = True)
+    print("Transaction added successfully")
+    print(df)
+    return df
+
 
 def edit_transaction(df: pd.DataFrame, index: int, **kwargs) -> pd.DataFrame:
     """
