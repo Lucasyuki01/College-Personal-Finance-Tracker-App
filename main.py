baa6f71f052e0035877ff4e36dd9ce2d36df4486
lucas-note <lucas.yuki@unesp.br>
2025-07-09 15:46:32 -0700

    Main menu done

--- main.py
+++ main.py
@@ -4,27 +4,59 @@ from budget_management import *
 from visualization import *
 
 def display_menu() -> None:
-    """Show menu options and prompt the user for a choice."""
-    pass
+    print("===== Personal Finance Tracker =====")
+    print()
+    
+    print("1. View All Transactions")
+    print("2. View Transactions by Date Range")
+    print("3. Add a Transaction")
+    print("4. Edit a Transaction")
+    print("5. Delete a Transaction")
+    print("6. Analyze Spending by Category")
+    print("7. Calculate Average Monthly Spending")
+    print("8. Show Top Spending Category")
+    print("9. Visualize Monthly Spending Trend")
+    print("10. Exit")
+    print()
 
-def handle_choice(choice: int, df, budgets) -> tuple[pd.DataFrame, dict[str, float]]:
-    """
-    Route to the correct function based on user choice:
-    import, view, add, edit, delete, analyze, budget, plot, etc.
-    Returns updated (df, budgets) pair.
-    """
-    pass
+def handle_choice(choice, df):
+    if choice == 1:
+        view_all_transactions()
+
+    elif choice == 2:
+        view_transactions_by_date()
+    
+    elif choice == 3:
+        add_transaction()
+
+    elif choice == 4:
+        edit_transaction()
+
+    elif choice == 5:
+        delete_transaction(df)
+
+    elif choice == 6:
+        analyse_spending__by_category()
+
+    elif choice == 7:
+        calculate_average_monthly()
+
+    elif choice == 8:
+        show_top_spending_category()
+
+    elif choice == 9:
+        visualize_monthly_spending_trend()
+
+    elif choice == 10:
+        exit()
 
 def main():
-    df = None
-    budgets: dict[str, float] = {}
+    df = pd.read_csv("sampledata.csv")
+
     while True:
         display_menu()
-        choice = int(input("Enter choice (0–9): "))
-        df, budgets = handle_choice(choice, df, budgets)
-        if choice == 9:
-            print("Exiting…")
-            break
+        choice = int(input(f"Choose an option (1-10): "))
+        handle_choice(choice, df)
 
 if __name__ == "__main__":
     main()
